ebdb48ffcb7b3a85e2bd87a0fa34e89c5bb84afe
Claude Ishimwe <dpqb12haikuo@gmail.com>
2023-04-05 16:25:26 +0200

    Make some changes and arrange some routes for resetting the password as well as the password reset token

--- model/userModel.js
+++ model/userModel.js
@@ -61,7 +61,7 @@ userSchema.methods.createPasswordResetToken = async function(){
                                 .createHash('sha256')
                                 .update(resetToken)
                                 .digest('hex');
-            console.log({resetToken}, this.passwordResetToken);
+            // console.log({resetToken}, this.passwordResetToken);
     this.passwordResetExpires = Date.now() + 10 * 60 *1000;
     return resetToken; 
 }
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -6,8 +6,8 @@ const router = express.Router();
 router.post('/signup', authController.signup)
 router.post('/login', authController.login)
 
-router.post('/forgot-password', authController.forgotPassword)
-//router.post('/resetPassword', authController.resetPassword)
+router.post('/forgot-password',authController.protect, authController.forgotPassword)
+router.post('/resetPassword/:token',authController.protect,  authController.resetPassword)
 
 
 
